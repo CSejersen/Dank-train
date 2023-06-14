81de87bdaceb96b409c19ed5625fcf7e69e61871
Christian <cvsejersen@gmail.com>
2023-06-14 14:46:38 +0200

    updated motordriver

--- Dank-Train/motordriver.cpp
+++ Dank-Train/motordriver.cpp
@@ -1,17 +1,17 @@
 #include "motorDriver.h"
 #include <avr/io.h> 
 
- // PE output and set to 0
 MotorDriver::MotorDriver(){
-	  DDRE = 0xFF;
-	  PORTE = 0;
-	  // Mode = 3 (PWM, Phase Correct, 10 bit)
-	  // Set OC3A on match down counting / Clear OC3A on match up counting - PE 3
-	  // Clock prescaler = 1
-	  TCCR3A = 0b10000011;
-	  TCCR3B = 0b00000011;
-	  // Duty cycle = 0%
-	  OCR3A = 0;
+    // PE output and set to 0
+    DDRE = 0xFF;
+    PORTE = 0;
+    // Mode = 3 (PWM, Phase Correct, 10 bit)
+    // Set OC3A on match down counting / Clear OC3A on match up counting - PE 3
+    // Clock prescaler = 1
+    TCCR3A = 0b10000011;
+    TCCR3B = 0b00000011;
+    // Duty cycle = 0%
+    OCR3A = 0;
 }
 
 void MotorDriver::forward(unsigned char speed){
@@ -36,7 +36,7 @@ void MotorDriver::backward(unsigned char speed){
         return;
     }
 
-     // PB ben 1 styrer retning på motor
+    // PB ben 1 styrer retning på motor
     PORTB &= 11111110;
 
     // Duty cycle sættes
